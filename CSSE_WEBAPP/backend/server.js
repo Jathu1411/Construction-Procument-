a3f02c5c241af177c5b60dc49ef698b0005dd31f
jathu <jathusanan@gmail.com>
2021-10-04 20:31:19 +0530

    mongodb connected

---
+++ CSSE_WEBAPP/backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 8095;
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(
+  MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (error) => {
+    if (error) {
+      console.log("Database Error " + error.message);
+    }
+  }
+);
+
+mongoose.connection.once("open", () => {
+  console.log("Database Synced");
+});
+
+app.listen(PORT, () => {
+  console.log(`DB is running on port ${PORT}`);
+});
